9d3ed6feab08e68f2f12e49b0b021f70c6d77934
Ben Bariteau <firba1@gmail.com>
2016-04-24 19:03:44 -0700

    combine tests for order by and All()

--- select_test.go
+++ select_test.go
@@ -29,42 +29,7 @@ func TestSelectStmtFirst(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestSelectStmtAll(t *testing.T) {
-	db := setupDB()
-
-	_, err := db.Exec("INSERT INTO tweet (id, text, likes) VALUES (?, ?, ?)", 1, "test tweet 1", 1)
-	assert.NoError(t, err)
-	_, err = db.Exec("INSERT INTO tweet (id, text, likes) VALUES (?, ?, ?)", 2, "test tweet 2", 2)
-	assert.NoError(t, err)
-	_, err = db.Exec("INSERT INTO tweet (id, text, likes) VALUES (?, ?, ?)", 3, "test tweet 3", 3)
-	assert.NoError(t, err)
-
-	tx, err := db.Begin()
-	assert.NoError(t, err)
-
-	dbtweets := make([]TweetRow, 0, 3)
-	err = Select(
-		tweetTable,
-	).All(tx, &dbtweets)
-
-	// TODO this is fragile since it depends on ordering
-	// either refactor to just care about values or just add ordering when it's implemented
-	assert.NoError(t, err)
-	assert.Equal(
-		t,
-		[]TweetRow{
-			TweetRow{1, Tweet{"test tweet 1", 1}},
-			TweetRow{2, Tweet{"test tweet 2", 2}},
-			TweetRow{3, Tweet{"test tweet 3", 3}},
-		},
-		dbtweets,
-	)
-
-	err = tx.Commit()
-	assert.NoError(t, err)
-}
-
-func TestSelectStmtOrderBy(t *testing.T) {
+func TestSelectStmtAllOrderBy(t *testing.T) {
 	db := setupDB()
 
 	_, err := db.Exec("INSERT INTO tweet (id, text, likes) VALUES (?, ?, ?)", 1, "test tweet 1", 1)
@@ -84,8 +49,6 @@ func TestSelectStmtOrderBy(t *testing.T) {
 		tweetTable.Likes.Asc(),
 	).All(tx, &dbtweets)
 
-	// TODO this is fragile since it depends on ordering
-	// either refactor to just care about values or just add ordering when it's implemented
 	assert.NoError(t, err)
 	assert.Equal(
 		t,
